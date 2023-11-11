09dd9a4d7839f29f5378c75d18006f2565ac3950
sunyu <2711783496@qq.com>
2023-11-11 12:39:46 +0800

    feat:用户微信登录、用户详情、修改用户信息

--- src/main/java/com/soft2242/shop/service/UserService.java
+++ src/main/java/com/soft2242/shop/service/UserService.java
@@ -1,7 +1,11 @@
 package com.soft2242.shop.service;
 
+import com.soft2242.shop.common.exception.ServerException;
 import com.soft2242.shop.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.soft2242.shop.query.UserLoginQuery;
+import com.soft2242.shop.vo.LoginResultVO;
+import com.soft2242.shop.vo.UserVO;
 
 /**
  * <p>
@@ -12,5 +16,18 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-11-08
  */
 public interface UserService extends IService<User> {
+    LoginResultVO login(UserLoginQuery query);
+
+//    根据用户id 获取用户信息
+//    @paran userId
+//    @return
+    User getUserInfo(Integer userId);
+
+//“修改用户信息
+//    @paran usaPVC
+//        @roturn
+    UserVO editUserInfo(UserVO userVO);
+
+
 
 }
