6dfc180b38231132d9af2e5bf074c7839db624c3
sunyu <2711783496@qq.com>
2023-11-25 11:31:11 +0800

    填写订单 - 获取再次购买订单

--- src/main/java/com/soft2242/shop/mapper/GoodsMapper.java
+++ src/main/java/com/soft2242/shop/mapper/GoodsMapper.java
@@ -2,6 +2,11 @@ package com.soft2242.shop.mapper;
 
 import com.soft2242.shop.entity.Goods;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.soft2242.shop.vo.UserOrderGoodsVO;
+import org.apache.ibatis.annotations.Param;
+
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +17,13 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2023-11-08
  */
 public interface GoodsMapper extends BaseMapper<Goods> {
+    /**
+     * 根据订单id 查询商品信息列表
+     *
+     * @param id
+     * @return
+     */
+    List<UserOrderGoodsVO> getGoodsListByOrderId(@Param("id") Integer id);
+
 
 }
--- src/main/java/com/soft2242/shop/service/UserOrderService.java
+++ src/main/java/com/soft2242/shop/service/UserOrderService.java
@@ -1,11 +1,17 @@
 package com.soft2242.shop.service;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.soft2242.shop.entity.Goods;
 import com.soft2242.shop.entity.UserOrder;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.soft2242.shop.query.OrderPreQuery;
 import com.soft2242.shop.vo.OrderDetailVO;
 import com.soft2242.shop.vo.SubmitOrderVO;
+import com.soft2242.shop.vo.UserOrderGoodsVO;
 import com.soft2242.shop.vo.UserOrderVO;
+import io.lettuce.core.dynamic.annotation.Param;
+
+import java.util.List;
 
 /**
  * <p>
@@ -46,4 +52,13 @@ public interface UserOrderService extends IService<UserOrder> {
      * @return
      */
     SubmitOrderVO getPreNowOrderDetail(OrderPreQuery query);
+
+    /**
+     * 填写订单 - 获取再次购买订单
+     *
+     * @param id
+     * @return
+     */
+    SubmitOrderVO getRepurchaseOrderDetail(Integer id);
+
 }
